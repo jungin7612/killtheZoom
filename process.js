8aea3e0da0b15fa693328232676707804c2f5e74
jungin7612 <jungin7612@gmail.com>
2021-06-09 19:43:02 +0900

    minute input 추가

--- process.js
+++ process.js
@@ -1,3 +1,18 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("수업 종료 뒤에 몇 분 후에 줌을 Kill하시겠습니까?? ", (minutes) => {
+  const settingMinutes = minutes;
+  console.log(`수업 시간 종료 후 줌 Kill : ${minutes}분 후`);
+
+  setInterval(() => {
+    scanDate(settingMinutes);
+  }, 10000);
+});
+
 const child_process = require("child_process");
 const killzoom = () => {
   child_process.execSync("powershell.exe -c 'taskkill /f /im Zoom.exe'");
@@ -7,27 +22,34 @@ const clock = () => {
 
   let hours = today.getHours(); // 시
   let minutes = today.getMinutes(); // 분
-  let seconds = today.getSeconds(); // 초
   const dateData = {
     hours: hours,
     minutes: minutes,
-    seconds: seconds,
   };
   return dateData;
 };
-const scanDate = () => {
+
+const scanDate = (settingMinutes) => {
   const data = clock();
 
-  if (data.hours < 8 || data.hours > 16) {
+  if (data.hours < 8 || data.hours > 20) {
+    console.log("아직 수업시간이 아닙니다.");
+    process.exit(1);
   } else if (data.hours >= 8 && data.hours < 13) {
-    if (data.minutes >= 52 && data.minutes < 55) {
-      killzoom();
+    if (data.minutes >= 50 + settingMinutes && data.minutes < 55) {
+      try {
+        killzoom();
+      } catch (err) {
+        console.log("zoom이 이미 꺼졌습니다");
+      }
     }
-  } else if (data.hours >= 13 && data.hours <= 16) {
-    if (data.minutes >= 43 && data.minutes < 45) {
-      killzoom();
+  } else if (data.hours >= 13 && data.hours <= 19) {
+    if (data.minutes >= 0 + settingMinutes && data.minutes < 45) {
+      try {
+        killzoom();
+      } catch (err) {
+        console.log("zoom이 이미 꺼졌습니다");
+      }
     }
   }
 };
-scanDate();
-setInterval(scanDate, 10000);
